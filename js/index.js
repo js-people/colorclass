da60147ef8567e5a34d84f005bd09faced5500c2
js-people <t56yu7@yahoo.com>
2015-12-01 13:18:06 +0330

    Updated to v-1.2

--- js/index.js
+++ js/index.js
@@ -5,76 +5,90 @@ $(document).ready(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)#\S+/g) || []).join(' ');
         }).addClass('#000000');
-		fColorClass();
+	cc_fColorClass(0, 0, 1);
 	})
+
     $("#BGwhite").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)#\S+/g) || []).join(' ');
         }).addClass('#ffffff');
-        fColorClass();
+        cc_fColorClass(0, 0, 1);
     })
+
     $("#BGred").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)#\S+/g) || []).join(' ');
         }).addClass('#ff0000');
-        fColorClass();
+        cc_fColorClass(0, 0, 1);
     })
+
     $("#BGblue").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)#\S+/g) || []).join(' ');
         }).addClass('#0000ff');
-        fColorClass();
+        cc_fColorClass(0, 0, 1);
     })
+
     $("#BGgreen").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)#\S+/g) || []).join(' ');
         }).addClass('#00ff00');
-        fColorClass();
+        cc_fColorClass(0, 0, 1);
     })
+
     $("#BGyellow").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)#\S+/g) || []).join(' ');
         }).addClass('#ffff00');
-        fColorClass();
+        cc_fColorClass(0, 0, 1);
     })
+
 
     // fast test Text
     $("#Tblack").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)@\S+/g) || []).join(' ');
         }).addClass('@000000');
-        fColorClass();
+        cc_fColorClass(0, 1, 0);
     })
+
     $("#Twhite").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)@\S+/g) || []).join(' ');
         }).addClass('@ffffff');
-        fColorClass();
+        cc_fColorClass(0, 1, 0);
     })
+
     $("#Tred").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)@\S+/g) || []).join(' ');
         }).addClass('@ff0000');
-        fColorClass();
+        cc_fColorClass(0, 1, 0);
     })
+
     $("#Tblue").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)@\S+/g) || []).join(' ');
         }).addClass('@0000ff');
-        fColorClass();
+        cc_fColorClass(0, 1, 0);
     })
+
     $("#Tgreen").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)@\S+/g) || []).join(' ');
         }).addClass('@00ff00');
-        fColorClass();
+        cc_fColorClass(0, 1, 0);
     })
+
     $("#Tyellow").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)@\S+/g) || []).join(' ');
         }).addClass('@ffff00');
-        fColorClass();
+        cc_fColorClass(0, 1, 0);
     })
+
+
+
 
 
     // fast test Background Opacity
@@ -82,20 +96,21 @@ $(document).ready(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)%\S+/g) || []).join(' ');
         }).addClass('%3');
-        fColorClass();
+        cc_fColorClass(0, 0, 1);
     }); 
+
     $("#opa6").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)%\S+/g) || []).join(' ');
         }).addClass('%6');
-        fColorClass();
+        cc_fColorClass(0, 0, 1);
     }); 
+
     $("#opa9").click(function() {
         $('.fast-test div').removeClass (function (index, css) {
             return (css.match (/(^|\s)%\S+/g) || []).join(' ');
         }).addClass('%9');
-        fColorClass();
+        cc_fColorClass(0, 0, 1);
     }); 
-
 
 });
